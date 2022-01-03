728ad9c63ee560498e0b6ab5200ff52240eea13b
jonghyeonbyun <225whdgus@gmail.com>
2022-01-03 19:08:23 +0900

    feat: apple transaction

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,89 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { Component, useEffect, useReducer, useState } from "react";
+import FruitshopContract from "./contracts/Fruitshop.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  const [myApple,setMyApple] = useState(0);
+  let initalState = {web3:null, instance:null, account:null};
+  const [state,dispatch] = useReducer(reducer, initalState);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
+  function reducer(state,action){
+    switch(action.type){
+      case "INIT":
+        let {web3, instance, account} = action
+        return {
+          ...state,
+          web3,
+          instance,
+          account
+        }
+      }
   }
+  const buyApple = async () => {
+    let {instance, account, web3} = state;
+    await instance.buyApple({
+      from: account,
+      value: web3.utils.toWei("10", "ether"),
+      gas : 4612388,
+    });
+    setMyApple(prev => prev + 1);
+  }
+
+  const sellApple = async () => {
+    let {instance, account, web3} = state;
+    await instance.sellApple(web3.utils.toWei("10","ether"), {
+      from: account,
+      gas : 4612388,
+    });
+    setMyApple(0);
+  }
+  
+  const getApple = async (instance) => {
+    if(instance == null) return;
+    let result = await instance.getMyApple();
+    setMyApple(result.toNumber());
+  }
+
+  const getweb = async () => {
+  const contract = require("@truffle/contract");
+
+    let web3 = await getWeb3();
+    let fruitshop = contract(FruitshopContract);
+    fruitshop.setProvider(web3.currentProvider);
+
+    let instance = await fruitshop.deployed();
+    // 계정 가져오기
+    let accounts = await web3.eth.getAccounts();
+
+    let InitActions = {
+      type : 'INIT',
+      web3,
+      instance,
+      account:accounts[0]
+    }
+
+    dispatch(InitActions);
+
+    getApple(instance);
+    // 내 계정 : 0x86d401DC2F798485D6Ea4885467A946b885fcD4B
+  
+  }
+
+  useEffect(()=> {
+    getweb();
+  }, []);
+
+  return(
+    <div>
+      <h1>사과 가격 : 10 ETH</h1>
+      <button onClick={()=>buyApple()}>BUY</button>
+      <p>내가 가지고 있는 사과 : {myApple}</p>
+      <button onClick={()=>sellApple()}>SELL (판매 가격은 : {myApple * 10} ETH)</button>
+    </div>
+  );
 }
 
 export default App;
